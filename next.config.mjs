7fca01b894c671bdeb392e8e78e1c3709a8caf58
sunainakharangate47 <sunaina@goaelectronics.co.in>
2025-04-05 08:11:17 +0530

    Fix assetPrefix in next.config.mjs to remove trailing slash for production

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
+  assetPrefix: isProd ? "/portfolio" : "",
 };
 
 export default nextConfig;
